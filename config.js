c07b770ac8bcc9059c2e3be479865f0ec336cddb
Janzz-Botzz <114346949+Janzz-Botzz@users.noreply.github.com>
2022-10-10 18:32:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'THE JANZ BOT'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6289517452071','6281226306638','60177426853']
+global.premium = ['6289517452071']
+global.packname = 'The Janz Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
